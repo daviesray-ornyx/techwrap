d4bbc1bd088674425393f516b50036ff1f2de228
Davies Ray <daviesray.ornyx@gmail.com>
2019-06-27 19:44:48 +0300

    Updated server IP address

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ def main():
 
 
 def start_server():
-    host = "192.168.100.3"
+    host = "4.14.146.42"
     port = 8000  # arbitrary non-privileged port
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
